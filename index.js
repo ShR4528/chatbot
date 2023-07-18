815cb0730f546d3377eec43a112521156885935a
Shr4528 <Shr4528@gmail.com>
2023-07-17 19:19:02 -0500

    api

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ dotenv.config();
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+    const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'user', content: 'what is capital of?' },
+        ],
+    });
+
+    console.log(chatCompletion);
+}
+main();
